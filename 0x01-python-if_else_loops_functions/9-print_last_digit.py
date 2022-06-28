480b068ed7e3821c9b6b9a2b4489d2c1791d4a6e
ALASHI1 <alashimuyiwa@gmail.com>
2022-06-28 16:20:07 +0100

    Update

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    print (int(str(number)[-1]))
+    print (int(str(number)[-1]),end="")
     return int(str(number)[-1])
+
+r = print_last_digit(98)
+print(r)
