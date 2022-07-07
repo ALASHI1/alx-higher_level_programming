1505c2158ce81d74d9be90dcc29b6339bf2fcb81
ALASHI1 <alashimuyiwa@gmail.com>
2022-07-07 01:21:38 +0100

    Update

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def square_matrix_simple(matrix=[]):
+    mutiples = []
+    new_matrix = map(lambda x: map(lambda y: y**2, x), matrix)
+    omo = list(new_matrix)
+    for i in omo:
+        oya = list(i)
+        mutiples.append(oya)
+    return mutiples
