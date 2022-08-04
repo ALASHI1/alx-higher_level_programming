5c7b96a041f097a0fec80e587aef1c524cd9529b
ALASHI1 <alashimuyiwa@gmail.com>
2022-08-04 01:17:24 +0100

    Update

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+1-mylist Module
+"""
+
+
 class MyList(list):
+    """A subclass of `list` object"""
+
     def print_sorted(self):
+        """prints the list, but sorted (ascending sort)"""
         print(sorted(self))
--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
-def is_same_class(obj, a_class):
+"""2-is_same_class
+"""
 
+
+def is_same_class(obj, a_class):
+    """checks if an object is exactly an instance of the specified class
+    Args:
+        obj: given object
+        a_class: given class
+    Return:
+        return True is obj is exactly an instance of a_class else False
+    """
     return type(obj) is a_class
