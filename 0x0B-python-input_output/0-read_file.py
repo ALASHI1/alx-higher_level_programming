dd96cae63a7f60efbb5d4d7f84eca639c2fceb2e
ALASHI1 <alashimuyiwa@gmail.com>
2022-08-04 01:31:08 +0100

    Update

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+0-read_file Module
+"""
+
+
+def read_file(filename=""):
+    """Read a text file (UTF8) and prints it to stdout
+    Args:
+        filename (string): name of text file
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        for line in f:
+            print(line, end="")
